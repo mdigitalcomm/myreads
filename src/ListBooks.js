fbf92f1242b0de79b09c5cb68e9b45ff2cc4a554
mdigitalcomm <mdigitalcomm@gmail.com>
2018-04-20 00:09:41 -0400

    added ListBooks Component

---
+++ src/ListBooks.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+class ListBooks extends Component {
+
+	render() {
+		let shelvedBooks = this.props.books.filter((obj)=> obj.shelf === this.props.shelf)
+		return (			
+				<div className="list-books">
+					<div className="list-books-content">
+						<div>
+							<div className="bookshelf">
+								<h2 className="bookshelf-title">
+									{this.props.shelf}
+								</h2>
+							</div>
+							
+							<div className="bookshelf-books">
+								<ol className="books-grid">
+									{shelvedBooks.map((book) => (	
+										<div key={book.title} className="book">			
+											<div className="book-top">	
+												<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}>
+												</div>
+												<div className="book-shelf-changer">
+													<select>
+						                                <option value="none" disabled>Move to...</option>
+						                                <option value="currentlyReading">Currently Reading</option>
+						                                <option value="wantToRead">Want to Read</option>
+						                                <option value="read">Read</option>
+						                                <option value="none">None</option>
+						                              </select>
+												</div>
+											</div>
+												<div className="book-title">{book.title}</div>
+												<div className="book-authors">{book.authors}</div>
+											
+										</div>
+										
+									))}
+								</ol>
+							</div>
+				        	<div className="open-search">
+				            	<Link to="/search">Add a book</Link>
+				          	</div>
+						</div>
+					</div>
+				</div>
+
+		)
+	}
+}
+
+export default ListBooks
