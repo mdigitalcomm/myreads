b043dd3842cec0da98c3723f4b4025952d8d5e37
mdigitalcomm <mdigitalcomm@gmail.com>
2018-04-20 00:48:50 -0400

    div tags adjustments

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+
 // import * as BooksAPI from './BooksAPI'
 import ListBooks from './ListBooks'
 import SearchBook from './SearchBook'
@@ -21,21 +23,29 @@ class BooksApp extends Component {
 
 
   render() {
-    
+
     return(
       <div className="app">
+        
         <div className="list-books-title">
             <h1>MyReads</h1>
         </div>
+        
         <Route exact path="/search" component={SearchBook} />
         
         <Route exact path="/" render={() => (
           <div className="list-books">
-            <ListBooks books={this.state.books} shelf="currentlyReading"/>
-            <ListBooks books={this.state.books} shelf="wantToRead"/>
-            <ListBooks books={this.state.books} shelf="read"/>        
+            <div className="list-books-content">
+              <ListBooks books={this.state.books} shelf="currentlyReading"/>
+              <ListBooks books={this.state.books} shelf="wantToRead"/>
+              <ListBooks books={this.state.books} shelf="read"/>
+              <div className="open-search">
+                <Link to="/search">Add a book</Link>
+              </div>
+            </div>    
           </div>   
-        )}/>   
+        )}/>
+
       </div>
     )
   }
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,50 +1,42 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
 
 class ListBooks extends Component {
 
 	render() {
 		let shelvedBooks = this.props.books.filter((obj)=> obj.shelf === this.props.shelf)
-		return (			
-				<div className="list-books">
-					<div className="list-books-content">
-						<div className="bookshelf">
-							<h2 className="bookshelf-title">
-								{this.props.shelf}
-							</h2>							
-							<div className="bookshelf-books">
-								<ol className="books-grid">
-									{shelvedBooks.map((book) => (	
-										<li key={book.title}>
-											<div className="book">			
-												<div className="book-top">	
-													<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}>
-													</div>
-													<div className="book-shelf-changer">
-														<select>
-							                                <option value="none" disabled>Move to...</option>
-							                                <option value="currentlyReading">Currently Reading</option>
-							                                <option value="wantToRead">Want to Read</option>
-							                                <option value="read">Read</option>
-							                                <option value="none">None</option>
-							                              </select>
-													</div>
-												</div>
-													<div className="book-title">{book.title}</div>
-													<div className="book-authors">{book.authors}</div>
-												
-											</div>
-										</li>
-										
-									))}
-								</ol>
-							</div>
-						</div>
-			        	<div className="open-search">
-			            	<Link to="/search">Add a book</Link>
-			          	</div>
-					</div>
+		return (												
+			<div className="bookshelf">
+				<h2 className="bookshelf-title">
+					{this.props.shelf}
+				</h2>							
+				<div className="bookshelf-books">
+					<ol className="books-grid">
+						{shelvedBooks.map((book) => (	
+							<li key={book.title}>
+								<div className="book">			
+									<div className="book-top">	
+										<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}>
+										</div>
+										<div className="book-shelf-changer">
+											<select>
+				                                <option value="none" disabled>Move to...</option>
+				                                <option value="currentlyReading">Currently Reading</option>
+				                                <option value="wantToRead">Want to Read</option>
+				                                <option value="read">Read</option>
+				                                <option value="none">None</option>
+				                              </select>
+										</div>
+									</div>
+										<div className="book-title">{book.title}</div>
+										<div className="book-authors">{book.authors}</div>
+									
+								</div>
+							</li>
+							
+						))}
+					</ol>
 				</div>
+			</div>			        										
 		)
 	}
 }
